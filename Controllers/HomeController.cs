35666d26bfc432cabc4bd1618899a1b5745a59b0
LNTTTruc2712 <lenguyentrungtruc2712@gmail.com>
2022-05-10 22:51:10 +0700

    WebDotNet

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using OnlineMarket.Models;
+using OnlineMarket.ModelViews;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly OnlineShopContext _context;
+
+
+        public HomeController(ILogger<HomeController> logger, OnlineShopContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            HomeViewVM model = new HomeViewVM();
+
+            var lsProducts = _context.Products
+                .AsNoTracking()
+                .Where(x => x.Active == true && x.HomeTag == true)
+                .OrderByDescending(x => x.DateCreated)
+                .ToList();
+
+            List<ProductHomeVM> lsProductViews = new List<ProductHomeVM>();
+
+            var lsCats = _context.Categories
+                .AsNoTracking()
+                .Where(x => x.Published == true)
+                .OrderByDescending(x => x.Ordering)
+                .ToList();
+
+            foreach(var item in lsCats)
+            {
+                ProductHomeVM productHome = new ProductHomeVM();
+                productHome.category = item;
+                productHome.lsProducts = lsProducts.Where(x => x.CatId == item.CatId).ToList();
+                lsProductViews.Add(productHome);
+            }
+
+            var TinTuc = _context.Posts
+                .AsNoTracking()
+                .Where(x => x.Published == true && x.IsNewFeed == true)
+                .OrderByDescending(x => x.CreateDate)
+                .Take(3)
+                .ToList();
+
+            model.Products = lsProductViews;
+            model.TinTucs = TinTuc;
+            ViewBag.AllProducts = lsProducts;
+
+            return View(model);
+        }
+
+        [Route("contact.html", Name = "Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("about.html", Name = "About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineMarket.Models;
+using PagedList.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly OnlineShopContext _context;
+        public ProductController(OnlineShopContext context)
+        {
+            _context = context;
+        }
+
+        [Route("shop.html", Name = "ShopProduct")]
+        public IActionResult Index(int? page)
+        {
+            try
+            {
+
+                var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+                var pageSize = 12;
+                var lsProducts = _context.Products.AsNoTracking().OrderByDescending(x => x.DateCreated);
+                PagedList<Product> models = new PagedList<Product>(lsProducts, pageNumber, pageSize);
+                ViewBag.CurrentPage = pageNumber;
+                return View(models);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+        [Route("/{Alias}", Name = "ListProduct")]
+        public IActionResult List(string Alias,int page = 1)
+        {
+            try
+            {
+                var pageSize = 10;
+                var categories = _context.Categories.AsNoTracking().SingleOrDefault(x => x.Alias == Alias);
+                var lsProducts = _context.Products
+                    .AsNoTracking()
+                    .Where(x => x.CatId == categories.CatId)
+                    .OrderByDescending(x => x.DateCreated);
+                PagedList<Product> models = new PagedList<Product>(lsProducts, page, pageSize);
+                ViewBag.CurrentPage = page;
+                ViewBag.CurrentCategory = categories;
+                return View(models);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+         
+        }
+
+        [Route("/{Alias}-{id}", Name = "ProductDetails")]
+        public IActionResult Details(int id)
+        {
+            try
+            {
+
+                var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+                if (product == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                var lsProduct = _context.Products
+                    .AsNoTracking()
+                    .Where(x => x.CatId == product.CatId && x.ProductId != id && x.Active == true)
+                    .OrderByDescending(x => x.DateCreated)
+                    .Take(4)
+                    .ToList();
+
+                ViewBag.SanPham = lsProduct;
+
+
+                return View(product);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+    }
+}
